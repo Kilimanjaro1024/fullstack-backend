2dedaeb80fdb37830a37dd6c648f31e99b41e7fe
Clayton Miller <clayton.miller@loop.colum.edu>
2021-02-02 17:39:24 -0600

    enables cors properly*

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const { PORT = 4000, NODE_ENV = "development" } = process.env;
 const mongoose = require("./db/connection");
-// const cors = require("cors");
-// const corsOptions = require("./configs/cors.js");
+const cors = require("cors");
+const corsOptions = require("./configs/cors.js");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
@@ -14,6 +14,7 @@ const wandRouter = require("./controllers/Wand.js");
 /////MIDDLEWARE///////
 // app.use(cors())
 app.use(express.urlencoded({extended:false}))
+NODE_ENV === "production" ? app.use(cors(corsOptions)) : app.use(cors());
 app.use(express.json())
 app.use(morgan())
 // ROUTE FOR TESTING SERVER IS WORKING
