e87cd368e10e339ac23ecbf78b3a4529296b7ef6
Clayton Miller <clayton.miller@loop.colum.edu>
2021-02-03 13:28:57 -0600

    adds route for adding a wand to a student

--- controllers/Student.js
+++ controllers/Student.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const Student = require('../models/Student')
+const Wand = require('../models/Wand')
 
 //get all
 router.get('/', async (req,res) => {
@@ -36,6 +37,20 @@ router.put('/id/:id', async (req,res) => {
             res.json(allStudents)
         }).catch(err => res.json({status: 400, err: err}))
     }).catch(err => res.json({status: 400, err: err}))
+})
+
+router.put('/id/:id/wand/:wandid', async (req,res) => {
+    Student.findByIdAndUpdate(req.params.id, req.body, {new: true}).then((student) =>{
+        Wand.findById(req.params.wandid).then((wand) => {
+            student.wands.push(wand._id)
+            student.save().then(() =>{
+                Student.find({}).populate("wands").then(allStudents => {
+                    res.json(allStudents)
+                }).catch(err => res.json({status: 400, err: err, location: "students.find"}))
+            }).catch(err => res.json({status: 400, err: err, location: "student.save"}))
+            
+        }).catch(err => res.json({status: 400, err: err, location: "wand.findbyid"}))
+    }).catch(err => res.json({status: 400, err: err, location: "students.findbyidandupdate"}))
 })
 
 //delete one
